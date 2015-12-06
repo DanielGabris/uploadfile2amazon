ffb762ee51bef554e802d9feaf7bc2769fc777a0
Kolek Schoonhoven <idealbuilder71@gmail.com>
2015-12-06 10:37:28 +0100

    changed entire code

---
+++ config/initializers/amazons3.rb
@@ -0,0 +1,5 @@
+Rails.application.config.amazons3 = {
+    aws_bucket_name: 'lezyeoh',
+    aws_secret_key: 'mOnJmdD+dHPfF9khhJ6lUopckfw41arkAOhOfg/v',
+    aws_secret_id: 'AKIAIEW5ODTJENQ2PM4Q'
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
-Uploadfile::Application.routes.draw do
+Rails.application.routes.draw do
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  root 'upload#index'
-  resources :upload
-  post 's3sign' => 'upload#s3sign'
+  root 'welcome#index'
+  get '/uploaded' => 'welcome#uploaded'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
